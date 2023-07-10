4a529df63ad303715f76efa938d3932f619e657d
pedro.gouveia <pedro.gouveia@magnifinance.com>
2023-07-10 23:22:38 +0100

    hotfix

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
          //baseURL: process.env.NEXT_PUBLIC_STRAPI_URL_API
-        //  baseURL: 'http://34.141.165.234:1337/api'
-        baseURL: 'https:beer2.me:1337/api'
+      baseURL: 'http://34.141.165.234:1337/api'
+      //  baseURL: 'https:beer2.me:1337/api'
 
 });
 export default api;
