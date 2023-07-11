14556b383576d259fbe157adcdb94740c6a69aea
pedro.gouveia <pedro.gouveia@magnifinance.com>
2023-07-11 13:05:20 +0100

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
          //baseURL: process.env.NEXT_PUBLIC_STRAPI_URL_API
-      baseURL: 'http://34.141.165.234:1337/api'
+      baseURL: 'https://beer2.me/pgv'
       //  baseURL: 'https:beer2.me:1337/api'
 
 });
