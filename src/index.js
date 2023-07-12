1e27a4783a68337930cf758aa424444b9d689b21
pedro.gouveia <pedro.gouveia@magnifinance.com>
2023-07-12 19:42:39 +0100

    add Gtag

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+    gtmId: 'GTM-KX8ZWNV'
+}
+
+TagManager.initialize(tagManagerArgs)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
